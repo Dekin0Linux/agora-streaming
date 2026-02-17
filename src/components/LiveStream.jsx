04584d974a054a4d2e9adf5f94f7ac6655642c61
faisaleganow <faisal@eganow.com>
2026-02-17 11:39:28 +0000

    pushing height

--- src/components/LiveStream.jsx
+++ src/components/LiveStream.jsx
@@ -11,9 +11,11 @@ export default function LiveStream() {
 
     const [joined, setJoined] = useState(false);
     const [role, setRole] = useState("host"); // host or audience
+    const [height,setHeight] = useState(300);
 
     useEffect(() => {
         clientRef.current = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
+        setHeight(window.innerHeight - 150);
     }, []);
 
     // join channel
@@ -74,28 +76,26 @@ export default function LiveStream() {
     };
 
     return (
-        <div style={{ padding: 20 }}>
-            <h2>Agora Live Streaming</h2>
+        <div style={{width : "100vw",paddingTop : "5px", display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center", height : "100vh",position:"relative" }}>
+            {/* <h2>Agora Live Streaming</h2> */}
 
             {!joined && (
-                <>
-                    <select onChange={(e) => setRole(e.target.value)}>
-                        <option value="host">Host (Go Live)</option>
+                <div style={{display : "flex", flexDirection : "row", justifyContent : "center", gap : "5px",position:"absolute",bottom:"10%",left:"50%",transform:"translate(-50%,0)"}}>
+                    <select onChange={(e) => setRole(e.target.value)} style={{padding : "10px", border : "1px solid #ccc", borderRadius : "5px"}}>
+                        <option value="host" >Host (Go Live)</option>
                         <option value="audience">Audience (Watch)</option>
                     </select>
-
-                    <br /><br />
                     <button onClick={joinChannel}>Join Stream</button>
-                </>
+                </div>
             )}
 
             {joined && (
-                <button onClick={leaveChannel}>Leave</button>
+                <button style={{padding : "10px", border : "1px solid #ccc", borderRadius : "5px", position:"absolute",bottom:"10%",left:"50%",transform:"translate(-50%,0)",backgroundColor:"red",color:"white"}} onClick={leaveChannel}>Leave</button>
             )}
 
             <div
                 ref={localVideoRef}
-                style={{ width: 400, height: 300, background: "#000", marginTop: 20 }}
+                style={{ width: 400, height: height, background: "#000", marginTop: 20 }}
             />
 
             <div id="remote-container" style={{ display: "flex", gap: 10, marginTop: 20 }} />
